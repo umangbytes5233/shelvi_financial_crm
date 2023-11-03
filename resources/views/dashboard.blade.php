c11e5a7d1ef21847304cb5016285d11f68c6ec1a
umangbytes5233 <114649337+199792-5233@users.noreply.github.com>
2023-11-03 15:48:40 +0530

    Needful Corrections_Login
    *Login Responsive
    *Small Design Changes Required added in form.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -101,7 +101,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-xl-9 d-flex">
+            <div class="col-xl-12 d-flex">
 
                 <div class="card flex-fill">
                     <div>
@@ -212,65 +212,7 @@
 
             </div>
 
-            <div class="col-xl-3 d-flex">
-                <div class="card flex-fill">
-                    <div class="card-header">
-                        <div class="d-flex justify-content-between align-items-center">
-                            <h5 class="card-title">Portfolio Management</h5>
-                            <div class="dropdown main">
-                                <button class="btn btn-white btn-sm dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Monthly
-                                </button>
-                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                    <li>
-                                        <a href="javascript:void(0);" class="dropdown-item">Weekly</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0);" class="dropdown-item">Monthly</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0);" class="dropdown-item">Yearly</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div id="invoice_chart"></div>
-                        <div class="text-center text-muted">
-                            <div class="row">
-                                <div class="col-4">
-                                    <div class="mt-4">
-                                        <p class="mb-2 text-truncate">
-                                            <i class="fas fa-circle text-primary me-1"></i>
-                                            Total Files
-                                        </p>
-                                        <h5>2,132</h5>
-                                    </div>
-                                </div>
-                                <div class="col-4">
-                                    <div class="mt-4">
-                                        <p class="mb-2 text-truncate">
-                                            <i class="fas fa-circle text-success me-1"></i>
-                                            Approved
-                                        </p>
-                                        <h5>1,763</h5>
-                                    </div>
-                                </div>
-                                <div class="col-4">
-                                    <div class="mt-4">
-                                        <p class="mb-2 text-truncate">
-                                            <i class="fas fa-circle text-danger me-1"></i>
-                                            Pending
-                                        </p>
-                                        <h5>973</h5>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          
         </div>
         <div class="row">
             <div class="col-md-6 col-sm-6">
